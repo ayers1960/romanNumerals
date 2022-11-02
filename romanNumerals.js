aec320cf25c93e8c39cb8ba9b505f61112d36406
ayers1960 <99854257+ayers1960@users.noreply.github.com>
2022-11-02 10:24:27 -0400

    Update romanNumerals.js

--- romanNumerals.js
+++ romanNumerals.js
@@ -14,12 +14,62 @@ function num2digits(num)
   return b
 }
 
+function digit2Roman(d)
+{
+  let retVal = "?"+d+"?"
+  if ( d < 4) {
+    retVal = "I".repeat(d)
+  }
+  else if ( d == 4) {
+    retVal = "IV"
+  }
+  else if ( d < 9 ) {
+     retVal = "V"+ "I".repeat(d-5)
+  }
+  else if ( d == 9) {
+    retVal = "IX"
+  }   
+  else if ( d < 40 ) {
+     retVal = "X".repeat(d/10)
+  }   
+ else if ( d < 90 ) {
+   console.log(d)
+     retVal = "L"+ "X".repeat((d-50)/10)
+  }   
+  else if ( d == 90) {
+    retVal = "XC"
+  }     
+ else if ( d < 400 ) {
+   console.log(d)
+     retVal = "C".repeat((d)/100)
+  }    
+ else if ( d == "500")    {
+   retVal = "D"
+ }
+  else if ( d < 900 ) {
+   console.log(d)
+     retVal = "D" + "C".repeat((d-500)/100)
+  }  
+  else if ( d == "900")    {
+   retVal = "DM"
+  }  
+  else if ( d < "4000")    {
+   retVal = "M".repeat(d/1000)
+  }    
+  return retVal
+}
+
 function convertToRoman(num) {
   let b = num2digits(num)
-  console.log(b)
- return num;
+  let roman = [];
+  console.log(b);
+  for(let i=0; i<b.length; i++)
+    roman.push(digit2Roman(b[i]))
+  console.log(roman);
+  let romNum = roman.join("")
+ return romNum;
 }
 
 console.log(
-convertToRoman(1)
+convertToRoman(3999)
 );

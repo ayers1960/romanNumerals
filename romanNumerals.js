686333d5a01bc52564c3a461451fe41f932605bd
ayers1960 <99854257+ayers1960@users.noreply.github.com>
2022-11-02 09:53:43 -0400

    Update romanNumerals.js

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,5 +1,25 @@
+
+function num2digits(num)
+{
+  let numStr = num.toString()
+  let a = numStr.split("")
+  let b = []
+
+  let decPlace = 1;
+  for(let i=a.length-1; i>=0; i--, decPlace *= 10)
+  {
+    b.unshift(a[i]*decPlace)
+  }
+  
+  return b
+}
+
 function convertToRoman(num) {
+  let b = num2digits(num)
+  console.log(b)
  return num;
 }
 
-convertToRoman(36);
+console.log(
+convertToRoman(1)
+);
